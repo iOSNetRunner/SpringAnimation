b04fb518cbf6d80b6d81e3cb1ad1d74132e16a28
NetRunner <galatskiy@gmail.com>
2023-06-14 00:37:17 +0300

    Initial Commit

---
+++ SpringAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringAnimation
+//
+//  Created by Dmitrii Galatskii on 14.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
